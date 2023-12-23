e548d85ebbc7388f625886c49334641c44af3f1e
alessandro benassi trenta <alessandro.benassitrenta@gmail.com>
2023-12-23 01:11:06 +0100

    added one test

---
+++ x-real-ip-overwriter_test.go
@@ -0,0 +1,71 @@
+package x_real_ip_overwrite
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/aless3/x-real-ip-overwrite"
+)
+
+# no changes
+func TestDemoNoCF(t *testing.T) {
+	cfg := x_real_ip_overwrite.CreateConfig()
+
+	ctx := context.Background()
+	next := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {})
+
+	handler, err := x-real-ip-overwrite.New(ctx, next, cfg, "x-real-ip-overwrite")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	recorder := httptest.NewRecorder()
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "http://localhost", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req.Headers["X-Real-IP"] = "127.0.0.1"
+	handler.ServeHTTP(recorder, req)
+
+	assertHeader(t, req, "X-Real-IP", "127.0.0.1")
+}
+
+# # apply changes
+# func TestDemoNoCF(t *testing.T) {
+# 	cfg := x_real_ip_overwrite.CreateConfig()
+
+# 	ctx := context.Background()
+# 	next := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {})
+
+# 	handler, err := x-real-ip-overwrite.New(ctx, next, cfg, "x-real-ip-overwrite")
+# 	if err != nil {
+# 		t.Fatal(err)
+# 	}
+
+# 	recorder := httptest.NewRecorder()
+
+# 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "http://localhost", nil)
+# 	if err != nil {
+# 		t.Fatal(err)
+# 	}
+
+# 	req.Headers["X-Real-IP"] = "127.0.0.1"
+#   req.Headers["CF-Connecting-IP"] = "192.168.0.0"
+# 	handler.ServeHTTP(recorder, req)
+
+# 	assertHeader(t, req, "X-Real-IP", "192.168.0.0")
+# 	assertHeader(t, req, "X-Real-IP-overwritten", "127.0.0.1")
+# }
+
+
+func assertHeader(t *testing.T, req *http.Request, key, expected string) {
+	t.Helper()
+
+	if req.Header.Get(key) != expected {
+		t.Errorf("invalid header value: %s", req.Header.Get(key))
+	}
+}
